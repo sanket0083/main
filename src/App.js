2133ec771b728c698ef2eb4215eb70602b68cebf
sanket0083 <sanketsavaliya0083@gmail.com>
2022-12-13 10:33:54 +0530

    merg table

--- src/App.js
+++ src/App.js
@@ -5,8 +5,30 @@ import data from './data.json';
 
 function App() {
 
+  const array1 = [{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	}];
+
+  const array2 = [{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	}];
+
+  const array = array1.concat(array2);
+
+  
 
 
+  
 
 
   return (
@@ -43,9 +65,28 @@ function App() {
               )
             })
           }
-          
-        </tbody>
+     </tbody>
       </table>
+      <table>
+        <tbody>
+          <tr>
+          <th>color</th>
+          <th>value</th>
+          </tr>
+          {
+            array.map((i) => {
+              return (
+                <tr key={Math.random()}>
+                  <td>{i.color}</td>
+                  <td>{i.value}</td>
+                </tr>
+              )
+            })
+          }
+     </tbody>
+      </table>
+      
+
     </div>
   );
 }
