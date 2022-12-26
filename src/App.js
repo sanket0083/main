c35607b73161e4db4e0553386ddba2fd08774443
sanket0083 <sanketsavaliya0083@gmail.com>
2022-12-26 09:18:38 +0530

    mounting component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react'
+import React , {useState , useEffect} from 'react'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-        name : 'sanket',
-        email : 'sanket@gmail.com',
-        student : {
-          name : 'student',
-          email : 'student@gmail.com'
-        },
-        age : 18
-      }
-    
-  }
-  btn = () => {
-    this.setState({name : 'kaushik', email : 'kaushik@gmail.com'})
-  }
+export const App = () => {
+  const [name ,setname] = useState('sanket')
 
-  render() {
-    return (
-      <div>
-        name is {this.state.name} <br />
-        name is {this.state.email} <br />
-        student name is {this.state.student.name} <br />
-        <button onClick={this.btn}>submit</button>
-        
-      </div>
-    )
-  }
+  // mounting component
+  useEffect(() => {
+    alert('savaliya');
+  },[])
+
+  return (
+    <div>
+
+      name is {name}
+    </div>
+  )
 }
+
+export default App
